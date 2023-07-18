6da50d95d2c8467f493fd42ac45c8743ba393b6c
mariya <mariyakudryashova19980308@gmail.com>
2023-07-18 20:12:04 +0300

    add dz04

---
+++ DZ04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int counter = 0;
+
+while (counter < number)
+{
+    if (counter % 2 == 0)
+    Console.WriteLine(counter);
+    counter++;
+}
