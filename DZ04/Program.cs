53f231a9ea7c228eaba527cc8d3ad18b122dda7c
mariya <mariyakudryashova19980308@gmail.com>
2023-07-18 20:13:55 +0300

    corr2

--- DZ04/Program.cs
+++ DZ04/Program.cs
@@ -5,7 +5,7 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int counter = 1;
 
-while (counter < number)
+while (counter <= number)
 {
     if (counter % 2 == 0)
     Console.WriteLine(counter);
